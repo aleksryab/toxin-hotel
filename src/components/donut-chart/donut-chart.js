430c13f4adc870fd46bf4d27385cb3ce23d51b0c
Aleksey <aleksryabtsev@gmail.com>
2020-03-07 16:00:58 +0700

    Add component donut-chart

---
+++ src/components/donut-chart/donut-chart.js
@@ -0,0 +1,140 @@
+import gradients from './gradients.xml';
+
+const donutChart = {
+  options: {
+    angleOffset: -90,
+    cx: '60',
+    cy: '60',
+    viewBox: '0 0 120 120',
+    radius: 56,
+    strokeWidth: 4,
+    gap: 3,
+    labelDeclensions: ['голосов', 'голос', 'голоса'],
+    colors: {
+      excellent: 'url(#gradient_excellent)',
+      good: 'url(#gradient_good)',
+      fair: 'url(#gradient_fair)',
+      poor: 'url(#gradient_poor)',
+      bad:  'black'
+    }
+  },
+  container: null,
+  svg: null,
+  group: null,
+  data: [],
+  totalValues: 0,
+  offset: 0,
+  circumference: 0,
+
+  createSvg() {
+    this.svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    this.svg.setAttribute('width', '100%');
+    this.svg.setAttribute('height', '100%');
+    this.svg.setAttribute('viewBox', this.options.viewBox);
+  },
+
+  createGroup() {
+    this.group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    this.group.setAttribute('transform', `rotate(${this.options.angleOffset} ${this.options.cx} ${this.options.cy})`);
+  },
+
+  createSegment(data) {
+    let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    circle.setAttribute('cx', this.options.cx);
+    circle.setAttribute('cy', this.options.cy);
+    circle.setAttribute('r', this.options.radius);
+    circle.setAttribute('fill', 'transparent');
+    circle.setAttribute('stroke', this.options.colors[data.rate]);
+    circle.setAttribute('stroke-width', this.options.strokeWidth);
+    circle.setAttribute('stroke-dashoffset', this.offset);
+    circle.setAttribute('stroke-dasharray', this.calculateSegment(data.value));
+
+    this.group.appendChild(circle);
+  },
+
+  createLegend() {
+    let textGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    textGroup.appendChild(this.createText('donut-chart__amount', this.totalValues));
+    textGroup.appendChild(this.createText('donut-chart__label', this.declension(this.totalValues)));
+    this.svg.appendChild(textGroup);
+  },
+
+  createText(cssClass, text) {
+    let textElement = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    textElement.setAttribute('x', '50%');
+    textElement.setAttribute('y', '50%');
+    textElement.classList.add(cssClass);
+    textElement.innerHTML = text;
+    return textElement;
+  },
+
+  calculateSegment(dataVal) {
+    let dash = dataVal/this.totalValues * this.circumference;
+    this.offset -= dash;
+    return `${dash - this.options.gap} ${this.circumference - dash + this.options.gap}`;
+  },
+
+  render() {
+    this.createSvg();
+    this.createGroup();
+
+    this.data.forEach(item => {
+      this.createSegment(item);
+    });
+
+    this.svg.innerHTML = gradients;
+    this.svg.appendChild(this.group);
+    this.createLegend();
+    this.container.appendChild(this.svg);
+  },
+
+  declension(val) {
+    switch (val) {
+      case 0:
+        return this.options.labelDeclensions[0];
+      case 1:
+        return this.options.labelDeclensions[1];
+      case 2:
+      case 3:
+      case 4:
+        return this.options.labelDeclensions[2];
+      default:
+        if (val > 20) {
+          if(val > 99) {
+            return this.declension(val % 100);
+          }
+          return this.declension(val % 10);
+        }
+        return this.options.labelDeclensions[0];
+    }
+  },
+
+  init(container) {
+
+    this.container = container;
+    this.data = JSON.parse(container.getAttribute('data-donut'));
+    container.removeAttribute('data-donut');
+
+    if(this.data.length == 1) {
+      this.options.gap = 0;
+    }
+
+    this.data.forEach(item => {
+      this.totalValues += item.value;
+    });
+
+    this.circumference = 2 * Math.PI * this.options.radius;
+
+    this.render();
+  },
+};
+
+$(document).ready(() => {
+  const donuts = document.querySelectorAll('.js-donut');
+  donuts.forEach(container => {
+    let objDonut = new Object;
+    objDonut.__proto__ = donutChart;
+    objDonut.init(container);
+  });
+
+});
